5a16824008a75e4551798264622989e3942c0346
Justyna <justyna.kurlowicz@gmail.com>
2017-07-20 22:49:15 +0200

    few api points added

--- api.js
+++ api.js
@@ -2,21 +2,16 @@
 
 var Router = require("koa-router");
 var dbModels = require("./models/dbTables");
-//var BodyParser = require("koa-bodyparser");
 
 const router = new Router()
 
-router.get('/videos', async (ctx, next) => {
+router.get("/videos", async (ctx, next) => {
   await dbModels.Video.fetchAll().then(function(results){
     ctx.body = results;
-    //ctx.body.toJSON({results.});
-    //ctx.response.toJSON({results}); 
   });
 });
 
-router.post('/videos', async (ctx,next) =>{
- // var data = await getBody(ctx.request);
- // console.log(data);
+router.post("/videos", async (ctx,next) =>{
   if(!ctx.request.body.title ||
       !ctx.request.body.description){
       console.log("puste parametry");      
@@ -30,13 +25,41 @@ router.post('/videos', async (ctx,next) =>{
   }
 });
 
+router.delete("/comments/:commentId", async (ctx,next) =>{
+    await dbModels.VideoComment.forge({id: ctx.params.commentId})
+    .fetch({require: true})
+    .then(function (comment){
+        comment.destroy();
+    });
+});
+
+router.get("/users/:userId/videos", async (ctx, next) => {
+    await dbModels.Video.query({where: {user_id: ctx.params.userId}})
+    .fetchAll({require: true})
+    .then(function (resData){
+        resData.forEach(function (model) {
+            console.log(model.attributes)
+        }) 
+    });
+});
+
+router.get("/users/:userId/comments", async (ctx, next) => {
+    await dbModels.VideoComment.query({where: {user_id: ctx.params.userId}})
+    .fetchAll({require: true})
+    .then(function (resData){
+        resData.forEach(function (model) {
+            console.log(model.attributes)
+        }) 
+    });
+});
+
 function getBody(ctx){
     return new Promise(function(resolve,reject){
-        var data = '';
-        ctx.on('data', function (chunk) {
+        var data = "";
+        ctx.on("data", function (chunk) {
             data += chunk;
         })
-        ctx.on('end', function (chunk) {
+        ctx.on("end", function (chunk) {
             resolve(data);
         })
     })
